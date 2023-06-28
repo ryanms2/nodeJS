4fa39d0d751e78f2350851c39c32495bd2aab226
ryanms2 <85030276+ryanms2@users.noreply.github.com>
2023-06-28 15:36:04 -0300

    começando à estruturar

---
+++ todolist-fullstack/backend/src/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+//router.get('/tasks', (req, res) => {
+ //   res.status(200).send('ROlá, Mundo!!!!');
+//});
+
+module.exports = router;
