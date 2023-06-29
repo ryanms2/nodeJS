18571c530c28dc191091d7092fc87add34037747
ryanms2 <85030276+ryanms2@users.noreply.github.com>
2023-06-29 17:45:49 -0300

    concluindo backend

--- todolist-fullstack/backend/src/router.js
+++ todolist-fullstack/backend/src/router.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const tasksController = require('./controllers/tasksControllers')
+const tasksController = require('./controllers/tasksControllers');
+const tasksMiddlewares = require('./middlewares/tasksMiddleware');
 const router = express.Router();
 
 router.get('/tasks', tasksController.getAll);
-
+router.post('/tasks', tasksMiddlewares.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id',
+ tasksMiddlewares.validateFieldTitle,
+ tasksMiddlewares.validateFieldStatus, 
+ tasksController.updateTask
+ );
+ 
 module.exports = router;
