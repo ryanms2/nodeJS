ab1eb55389f39d6a523d4c203d8dfff0b1ac8348
ryanms2 <85030276+ryanms2@users.noreply.github.com>
2023-06-30 21:07:57 -0300

    added cors

--- todolist-fullstack/backend/src/app.js
+++ todolist-fullstack/backend/src/app.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const router = require('./router');
 
 const app = express(); 
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 module.exports = app;
