cb4d37bb0318767071f519b61476cabe49f8c9b7
ryanms2 <85030276+ryanms2@users.noreply.github.com>
2023-07-08 16:39:38 -0300

    add consulta e inserção

--- orcamentoF/backend/app.js
+++ orcamentoF/backend/app.js
@@ -12,5 +12,10 @@ const conn = require("./src/db/conn");
 
 conn();
 
+// routes
+const routes = require("./src/routes/router");
+
+app.use("/api", routes);
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`servidor rodando na porta ${PORT}`));
---
+++ orcamentoF/backend/src/routes/services.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+const serviceController = require("../controllers/serviceController");
+
+router.route("/services")
+.post((req, res) => serviceController.create(req, res));
+
+router.route("/services").get((req, res) => serviceController.getAll(req, res));
+
+router.route("/services/:id").get((req, res) => serviceController.get(req, res));
+
+module.exports = router;
