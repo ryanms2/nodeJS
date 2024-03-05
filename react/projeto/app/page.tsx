11050c3e0844264f9de3c8ec5f20608d017d025f
Rian Santos <riansantos355@gmail.com>
2024-03-05 15:43:10 -0300

    estudo typescript

--- react/projeto/app/page.tsx
+++ react/projeto/app/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { FormEvent, useState } from 'react';
 import './page.css'
 
 export default function Home() {
+  function enviar(e:FormEvent) {
+    e.preventDefault()
+    console.log(email)
+  }
+
+  const john: string = "aaaa"
+  
+  const [email, setEmail] = useState();
+
   return (
     <div className='layout'>
-      <h2>Olá mundo!!!</h2>
+      <div className='container_login'>
+        <form onSubmit={enviar}>
+          <div>
+            <label htmlFor="email">email</label>
+            <input type="email" name='email' id='email' onChange={(e: any) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="password">Senha</label>
+            <input type="password" name='password' id='password' />
+          </div>
+          
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ react/projeto/app/painel/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>page do painel</h1>
+    )
+}
